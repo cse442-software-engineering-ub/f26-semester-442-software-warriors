de14efc81a8f7cb8f614cbe26f1b7d293ddf097b
Merveilles <mbkatumb@buffalo.edu>
2026-09-21 21:42:16 -0400

    Remove unused imports in App.tsx blocking production build

--- vitals-webapp/src/App.tsx
+++ vitals-webapp/src/App.tsx
@@ -1,15 +1,8 @@
-import { useState } from 'react'
-import heroImg from './assets/hero.png'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import './App.css'
-import RegisterPage from './pages/RegisterPage';
+import "./App.css";
+import RegisterPage from "./pages/RegisterPage";
 
 function App() {
-
-  return (
-      <RegisterPage/>
-  )
+  return <RegisterPage />;
 }
 
-export default App
+export default App;
